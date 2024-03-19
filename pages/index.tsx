f06d36d17f44d79ee10024b20efc9da08fca973f
francisco matheus <matheusvale554@gmail.com>
2024-03-18 21:22:51 -0300

    Configuração do projeto, configuração dos arquivos e instalação das libs que serão usadas

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import styles from "../styles/homeNoAuth.module.scss"
+
+const HomeNotAuth = function () {
+  return (
+		<>
+			<Head>
+                <title>home</title>
+            </Head>
+
+			<main></main>
+		</>
+  )};
+
+export default HomeNotAuth;
