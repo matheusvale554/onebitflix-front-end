ddd731a3465537737775b3f7f7b6b48626862219
francisco matheus <matheusvale554@gmail.com>
2024-03-31 18:17:09 -0300

    criação e configuração da pagina de login e criação da autenticação

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -8,6 +8,11 @@ interface RegisterParams {
   email: string;
   password: string;
 }
+interface LoginParams {
+  email: string;
+  password: string;
+}
+
 
 const authService = {
     register: async (params: RegisterParams) => {
@@ -21,6 +26,22 @@ const authService = {
   
       return res;
     },
+    login: async (params: LoginParams) => {
+      const res = await api.post("/auth/login", params).catch((error) => {
+      if (error.response.status === 400 || error.response.status === 401) {
+        return error.response;
+      }
+      return error;
+      });
+    
+      if (res.status === 200) {
+        sessionStorage.setItem("onebitflix-token", res.data.token);
+      }
+    
+      return res;
+    }
   };
   
   export default authService;
+
+  
