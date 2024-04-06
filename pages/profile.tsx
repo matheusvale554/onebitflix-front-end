f0892a7603581f01138e3f12e77a49daf86f8c69
francisco matheus <matheusvale554@gmail.com>
2024-04-06 14:14:04 -0300

    criação e estilização da area profile do usuario

---
+++ pages/profile.tsx
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import styles from "../styles/profile.module.scss";
+import UserFrom from "@/components/profile/user";
+import HeaderAuth from "@/components/homeAuth";
+import { Button, Col, Container, Row } from "reactstrap";
+import Footer from "@/components/common/footer";
+
+const UserInfo = function () {
+  return (
+    <>
+      <Head>
+        <title>Onebitflix - Meus Dados</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+      </Head>
+      <main>
+        <div className={styles.header}>
+        <HeaderAuth/>
+        </div>
+        
+        <Container calssName="py-5">
+		<p className={styles.title}>Minha Conta</p>
+    <Row className="pt-3 pb-5">
+	    <Col md={4} className={styles.btnColumn}>
+	      <Button outline className={styles.renderFormBtn}>
+	        DADOS PESSOAIS
+        </Button>
+        <Button outline className={styles.renderFormBtn}>
+	        SENHA
+        </Button>
+      </Col>
+      <Col md>
+	      <UserFrom />
+      </Col>
+     </Row>
+   </Container>
+   <div className={styles.footer}>
+   <Footer />
+</div>
+      </main>
+    </>
+  );
+};
+
+export default UserInfo;
